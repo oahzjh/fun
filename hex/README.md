4e80e583bde5bf7595587dfcafd844ebe87188f1
James Zhao <hjz@hackjumpzero.ca>
2018-10-14 13:48:48 -0400

    Initial hex

---
+++ hex/README.md
@@ -0,0 +1,6 @@
+# Hex (Personalized binary file editor)
+
+- Pattern searching
+- Fast editing
+- Handle large files
+- Statistical analysis
---
+++ hex/hex.py
@@ -0,0 +1,11 @@
+""" 
+This is a python implementaiton of hex editor.
+"""
+import os
+import sys
+
+def main(argv):
+    pass
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
